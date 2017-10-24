15b3ddda27ed72ae34aa0f47185cf0ff0ad782a4
loicmagny <loicmagn60@gmail.com>
2017-10-24 10:03:58 +0200

    ex6js

---
+++ ex6js.js
@@ -0,0 +1,5 @@
+var number1, number2, result;
+number1 = prompt('Entrez le premier chiffre :');
+number2 = prompt('Entrez le second chiffre :');
+result = parseInt(number1) % parseInt(number2);
+alert(result);
